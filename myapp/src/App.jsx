39b87eece10ab3f55e97aacdbee1b5d9ceb371f0
Fabrizio <pala.fabrizio@live.it>
2024-02-02 19:16:35 +0100

    1

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,14 +1,35 @@
+
+import React, { useState } from 'react';
 import './App.css'
-import { TodoList } from './TodoList'
+
+import { Clock } from './Clock';
+import { LanguageContext } from './LanguageContext';
 
 function App() {
-  const list = ["ice", "fire", "earth", "pino"]
+  const [selectedLanguage, setSelectedLanguage] = useState("eng");
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
 
   return (
-    <>
-    <TodoList list={list}></TodoList>
-    </>
-  )
+    <div>
+      <label htmlFor="language">Select Language: </label>
+      <select
+        name="language"
+        value={selectedLanguage}
+        onChange={handleLanguageChange}
+      >
+        <option value="eng">English</option>
+        <option value="ita">Italiano</option>
+        {/* Add more language options as needed */}
+      </select>
+
+      <LanguageContext.Provider value={selectedLanguage}>
+        <Clock />
+      </LanguageContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
